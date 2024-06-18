f119f77e5a9f88f44b7b6c3d7b6a4a31573c0b30
KASUVUPRAVEENKUMAR <164033432+KASUVUPRAVEENKUMAR@users.noreply.github.com>
2024-06-18 21:55:51 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,25 @@
+Select operation:
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+Enter choice (1/2/3/4): 1
+Enter first number: 23
+Enter second number: 33
+23.0 + 33.0 = 56.0
+Do you want to do another calculation? (yes/no): yes
+Enter choice (1/2/3/4): 2
+Enter first number: 54
+Enter second number: 21
+54.0 - 21.0 = 33.0
+Do you want to do another calculation? (yes/no): yes
+Enter choice (1/2/3/4): 3
+Enter first number: 12
+Enter second number: 12
+12.0 * 12.0 = 144.0
+Do you want to do another calculation? (yes/no): yes
+Enter choice (1/2/3/4): 4
+Enter first number: 36
+Enter second number: 10
+36.0 / 10.0 = 3.6
+Do you want to do another calculation? (yes/no): no
